73919ff0d69e36648bf3ce23e310d7d11067f5c3
AngeloChen <angelochen@live.cn>
2021-10-19 18:27:39 +0800

    fix for new test

--- src/AngleCalculator.cpp
+++ src/AngleCalculator.cpp
@@ -127,7 +127,7 @@ double RosAngleCalculator::calculateAngle(const visualization_msgs::MarkerArray&
   
   if(target_flag)
   {
-    ROS_INFO_STREAM("Target position is:"<<target_pose_.pose.position);
+    //ROS_INFO_STREAM("Target position is:"<<target_pose_.pose.position);
     target_angle = calculateAngle_Target(target_pose_);
     setNavGoal(target_pose_);
     // count++;
@@ -190,7 +190,7 @@ double RosAngleCalculator::calculateAngle_Target(const geometry_msgs::PoseStampe
   double x=0, y=0, target_angle=0;
   try
   {
-    transformStamped = tfBuffer_.lookupTransform("base_link", target_message.header.frame_id, ros::Time(0),ros::Duration(0.1));
+    transformStamped = tfBuffer_.lookupTransform("base_link", target_message.header.frame_id, target_message.header.stamp,ros::Duration(0.1));
     tf2::doTransform(target_message,pose_out,transformStamped);
   }
   catch (tf2::TransformException &ex) 
@@ -251,7 +251,7 @@ double RosAngleCalculator::calculateScore_Taget(const geometry_msgs::PoseStamped
   geometry_msgs::PoseStamped pose_out;
   try
   {
-    transformStamped = tfBuffer_.lookupTransform("camera_visor", target_message.header.frame_id, ros::Time(0),ros::Duration(0.5));
+    transformStamped = tfBuffer_.lookupTransform("camera_visor", target_message.header.frame_id, target_message.header.stamp,ros::Duration(0.1));
      tf2::doTransform(target_message,pose_out,transformStamped);
   }
   catch (tf2::TransformException &ex) 
